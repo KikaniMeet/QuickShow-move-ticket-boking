7bc7d4060ed22f91daa61fcae50430c9155a5ebd
KikaniMeet <mitkikani027@gmail.com>
2025-06-24 23:22:16 +0530

    varisu.jsonchanges

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,26 +1,26 @@
 import User from "../models/User.js"; 
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "movie-ticket-booking" });  // Fixed typo
+export const inngest = new Inngest({ id: "movie_ticket_booking" });
 
 const syncUserCreation = inngest.createFunction(
-  { id: 'sync-user-creation' },  // Unique ID
-  { event: 'clerk/user.created' },
+  { id: "sync_user_creation" },
+  { event: "clerk/user.created" },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const userData = {
       _id: id,
-      email: email_addresses[0].email_address,  // Fixed property
+      email: email_addresses[0].email_address,
       name: `${first_name} ${last_name}`.trim(),
       image: image_url
     };
-    await User.create(userData);  // Fixed space
+    await User.create(userData);
   }
 );
 
 const syncUserDeletion = inngest.createFunction(
-  { id: 'sync-user-deletion' },  // Unique ID
-  { event: 'clerk/user.deleted' },
+  { id: "sync_user_deletion" },
+  { event: "clerk/user.deleted" },
   async ({ event }) => {
     const { id } = event.data;
     await User.findByIdAndDelete(id);
@@ -28,20 +28,16 @@ const syncUserDeletion = inngest.createFunction(
 );
 
 const syncUserUpdation = inngest.createFunction(
-  { id: 'sync-user-updation' },  // Unique ID
-  { event: 'clerk/user.updated' },
+  { id: "sync_user_updation" },
+  { event: "clerk/user.updated" },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const userData = {
-      email: email_addresses[0].email_address,  // Fixed property
+      email: email_addresses[0].email_address,
       name: `${first_name} ${last_name}`.trim(),
       image: image_url
     };
-    await User.findByIdAndUpdate(  // Fixed method name
-      id, 
-      userData,
-      { new: true }  // Returns updated document
-    );
+    await User.findByIdAndUpdate(id, userData, { new: true });
   }
 );
 
