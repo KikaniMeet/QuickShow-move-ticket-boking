a569835212e7a0d7aac32baa6bccf5458fb33a38
KikaniMeet <mitkikani027@gmail.com>
2025-07-04 12:34:48 +0530

    client

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-    optimizeDeps: {
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',  // 👈 this is the fix for Render
+    port: 5173        // optional: set your preferred dev port
+  },
+  optimizeDeps: {
     force: true
   }
 })
