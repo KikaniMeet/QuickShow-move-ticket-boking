c9f090c6993ec98f61b9027f653d8cf3cb095af8
KikaniMeet <mitkikani027@gmail.com>
2025-07-05 00:03:12 +0530

    server

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const startServer = async () => {
     app.get('/', (req, res) => res.send('Server is Live!'));
     app.use('/api/inngest', serve({ client: inngest, functions }));
     app.use('/api/show', showRouter);
-    app.use('/api/booking', bookingRouter);
+    app.use('/api/booking',bookingRouter);
     app.use('/api/admin', adminRouter);
     app.use('/api/user', userRouter);
 
