ea1b0104c2535c6427b7317fc1891dc44dfb340f
KikaniMeet <mitkikani027@gmail.com>
2025-07-07 02:17:30 +0530

    ALL

--- server/Routes/bookingRoutes.js
+++ server/Routes/bookingRoutes.js
@@ -3,14 +3,14 @@
 import express from 'express';
 import { createBooking, getOccupiedSeats } from '../controllers/bookingController.js';
 
-const tempRouter = express.Router();
+const bookingRoutes = express.Router();
 
 // @route   POST /api/booking/create
 // @desc    Create a new booking
-tempRouter.post('/create', createBooking);
+bookingRoutes.post('/create', createBooking);
 
 // @route   GET /api/booking/seats/:showId
 // @desc    Get occupied seats for a show
-tempRouter.get('/seats/:showId', getOccupiedSeats);
+bookingRoutes.get('/seats/:showId', getOccupiedSeats);
 
-export default tempRouter;
+export default bookingRoutes;
