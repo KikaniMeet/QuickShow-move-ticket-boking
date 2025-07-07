543475817e930edc3b6f50b52d88a911885fd179
KikaniMeet <mitkikani027@gmail.com>
2025-07-07 14:29:40 +0530

    server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
-import bookingRoutes from './routes/bookingRoutes.js';
+import bookingRoutes from './routes/bokingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js'
 import showRouter from './routes/showRoutes.js'; 
